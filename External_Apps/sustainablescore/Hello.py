ca22b308f71d62ff10e40e64c0f25e9d008108c3
Abdullah Masood <119092945+abd766@users.noreply.github.com>
2024-12-17 00:06:22 +0530

    Update Hello.py

--- External_Apps/sustainablescore/Hello.py
+++ External_Apps/sustainablescore/Hello.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-import json
 from inference import run_inference
 
-# Define function to scrape Amazon product page
 def scrape_amazon(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -38,17 +36,8 @@ def main():
             response = run_inference(prompt)
             st.balloons()
 
-            if isinstance(response, dict):
-                st.header('ESG Score: ')
-                st.write(response.get('esg_score', 'N/A'))
-
-                st.header('Pros: ')
-                for key, value in response.get('pros', {}).items():
-                    st.write(f'{key}. {value}')
-
-                st.header('Cons: ')
-                for key, value in response.get('cons', {}).items():
-                    st.write(f'{key}. {value}')
+            if isinstance(response, str):
+                st.markdown(response)  # Display the sustainability analysis
             else:
                 st.error(response)
         else:
